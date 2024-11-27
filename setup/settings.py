4a645373c1dcc1d4426a347934afc18448de99f6
André Arruda <andre23arruda@gmail.com>
2024-11-27 15:06:50 -0300

    fix: Update static url

--- setup/settings.py
+++ setup/settings.py
@@ -117,8 +117,10 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
