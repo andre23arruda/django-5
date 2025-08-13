fdade709c65ac6fdc3aa631a2e82c0f35c220398
andre23arruda <andre23arruda@gmail.com>
2025-08-13 14:44:13 -0300

    feat: Enhance dark mode styles and add game creation button in tournament forms

--- setup/static/js/create-games-modal.js
+++ setup/static/js/create-games-modal.js
@@ -1,10 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var updateButton = document.getElementById('create-games')
-    var cancelButton = document.getElementById('games-cancel')
-    var gamesDialog = document.getElementById('games-dialog')
+    const canCreateGames = document.getElementById('canCreateGames');
+    const jogosTab = document.getElementById('jogos-tab');
+    if (jogosTab && canCreateGames) {
+        jogosTab.innerHTML += `
+            <button
+                id="create-games"
+                type="button"
+                class="btn btn-block btn-primary btn-sm mt-2"
+                style="
+                    position: absolute;
+                    top: 10px;
+                    right: 20px;
+                    max-width: 100px;
+                "
+            >
+                Gerar Jogos
+            </button>
+        `
+    }
 
-    if (updateButton) {
-        updateButton.addEventListener('click', function () {
+    const gamesButton = document.getElementById('create-games')
+    const cancelButton = document.getElementById('games-cancel')
+    const gamesDialog = document.getElementById('games-dialog')
+
+    if (gamesButton && cancelButton && gamesDialog) {
+        gamesButton.addEventListener('click', function () {
             gamesDialog.showModal()
         })
 
