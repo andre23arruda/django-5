abd9d6f9059b52acf4b9aced3e65776dcb520918
andre23arruda <andre23arruda@gmail.com>
2025-08-23 13:39:43 -0300

    feat: Add tests for player creation and points calculation in Jogador model

--- apps/bt_cup/tests.py
+++ apps/bt_cup/tests.py
@@ -1,3 +1,113 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Dupla, Torneio, Jogo
 
-# Create your tests here.
+class DuplaTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='12345'
+        )
+        self.dupla = Dupla.objects.create(
+            jogador1='Jogador 1',
+            jogador2='Jogador 2',
+            telefone='123456789',
+            criado_por=self.user
+        )
+
+    def test_dupla_creation(self):
+        self.assertTrue(isinstance(self.dupla, Dupla))
+        self.assertEqual(
+            self.dupla.__str__(),
+            'Jogador 1/Jogador 2'
+        )
+        self.assertTrue(self.dupla.ativo)
+
+    def test_render_method(self):
+        self.assertEqual(
+            self.dupla.render(),
+            'Jogador 1<br/>Jogador 2'
+        )
+
+
+class TorneioTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='12345'
+        )
+        self.torneio = Torneio.objects.create(
+            nome='Torneio Teste',
+            data='2022-01-01',
+            criado_por=self.user,
+            quantidade_grupos=2
+        )
+        # Create test doubles
+        self.duplas = []
+        for i in range(8):  # Create 8 doubles for 2 groups
+            dupla = Dupla.objects.create(
+                jogador1=f'Jogador {i*2+1}',
+                jogador2=f'Jogador {i*2+2}',
+                criado_por=self.user
+            )
+            self.duplas.append(dupla)
+            self.torneio.duplas.add(dupla)
+
+    def test_torneio_creation(self):
+        self.assertTrue(isinstance(self.torneio, Torneio))
+        self.assertEqual(self.torneio.__str__(), 'Torneio Teste')
+        self.assertTrue(self.torneio.ativo)
+
+    def test_create_games(self):
+        self.torneio.create_games()
+        games_count = Jogo.objects.filter(
+            torneio=self.torneio,
+            fase__startswith='GRUPO'
+        ).count()
+        expected_games = 12
+        self.assertEqual(games_count, expected_games)
+
+    def test_game_status_concluido(self):
+        self.torneio.create_games()
+        game = Jogo.objects.filter(
+            torneio=self.torneio,
+            fase__startswith='GRUPO'
+        ).first()
+        game.placar_dupla1 = 6
+        game.placar_dupla2 = 7
+        game.save()
+        self.assertTrue(game.concluido == 'C')
+
+    def test_game_winner(self):
+        self.torneio.create_games()
+        game = Jogo.objects.filter(
+            torneio=self.torneio,
+            fase__startswith='GRUPO'
+        ).first()
+        game.placar_dupla1 = 6
+        game.placar_dupla2 = 7
+        game.save()
+        self.assertEqual(game.winner, game.dupla2)
+
+    def test_get_groups_ranking(self):
+        self.torneio.create_games()
+        games = Jogo.objects.filter(torneio=self.torneio)
+        game1 = games[0]
+        game1.placar_dupla1 = 21
+        game1.placar_dupla2 = 19
+        game1.concluido = 'C'
+        game1.save()
+
+        game2 = games[1]
+        game2.placar_dupla1 = 10
+        game2.placar_dupla2 = 9
+        game2.concluido = 'C'
+        game2.save()
+
+        ranking = self.torneio.get_groups_ranking()
+        self.assertTrue('GRUPO 1' in ranking)
+        self.assertTrue('GRUPO 2' in ranking)
+        group1_ranking = ranking['GRUPO 1']
+        self.assertTrue(game1.dupla1 == group1_ranking[0]['dupla'])
+        self.assertTrue(game2.dupla1 == group1_ranking[1]['dupla'])
+        self.assertTrue(group1_ranking[0]['pontos'] > group1_ranking[1]['pontos'])
--- apps/bt_league/tests.py
+++ apps/bt_league/tests.py
@@ -1,3 +1,111 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Jogador, Torneio, Jogo
 
-# Create your tests here.
+class JogadorTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='12345'
+        )
+        self.jogador = Jogador.objects.create(
+            nome='Jogador Teste',
+            telefone='123456789',
+            criado_por=self.user
+        )
+
+    def test_player_creation(self):
+        self.assertTrue(isinstance(self.jogador, Jogador))
+        self.assertEqual(self.jogador.__str__(), 'Jogador Teste')
+        self.assertTrue(self.jogador.ativo)
+
+    def test_player_points(self):
+        torneio = Torneio.objects.create(
+            nome='Torneio Teste',
+            data='2022-01-01',
+            criado_por=self.user
+        )
+        jogador2 = Jogador.objects.create(
+            nome='Jogador 2',
+            criado_por=self.user
+        )
+        jogador3 = Jogador.objects.create(
+            nome='Jogador 3',
+            criado_por=self.user
+        )
+        jogador4 = Jogador.objects.create(
+            nome='Jogador 4',
+            criado_por=self.user
+        )
+
+        # Create a game where player wins
+        jogo = Jogo.objects.create(
+            torneio=torneio,
+            quadra=1,
+            dupla1_jogador1=self.jogador,
+            dupla1_jogador2=jogador2,
+            dupla2_jogador1=jogador3,
+            dupla2_jogador2=jogador4,
+            placar_dupla1=21,
+            placar_dupla2=19,
+        )
+        vitorias, pontos, saldo, n_jogos = self.jogador.player_points(torneio)
+        self.assertEqual(vitorias, 1)
+        self.assertEqual(pontos, 21)
+        self.assertEqual(saldo, 2)
+        self.assertEqual(n_jogos, 1)
+        self.assertEqual('C', jogo.concluido)
+
+
+class TorneioTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='12345'
+        )
+        self.torneio = Torneio.objects.create(
+            nome='Torneio Teste',
+            data='2022-01-01',
+            criado_por=self.user
+        )
+        self.player1 = Jogador.objects.create(
+            nome='Jogador 1',
+            criado_por=self.user
+        )
+        self.player2 = Jogador.objects.create(
+            nome='Jogador 2',
+            criado_por=self.user
+        )
+        self.player3 = Jogador.objects.create(
+            nome='Jogador 3',
+            criado_por=self.user
+        )
+        self.player4 = Jogador.objects.create(
+            nome='Jogador 4',
+            criado_por=self.user
+        )
+
+    def test_tournament_creation(self):
+        self.assertTrue(isinstance(self.torneio, Torneio))
+        self.assertEqual(self.torneio.__str__(), 'Torneio Teste')
+        self.assertTrue(self.torneio.ativo)
+
+    def test_create_games(self):
+        self.torneio.jogadores.add(self.player1, self.player2, self.player3, self.player4)
+        self.torneio.create_games()
+        self.assertEqual(Jogo.objects.filter(torneio=self.torneio).count(), 3)
+        self.assertIsNone(Jogo.objects.filter(torneio=self.torneio).first().placar_dupla1)
+
+    def test_no_game_with_insufficient_players(self):
+        self.torneio.jogadores.add(self.player1, self.player2)
+        self.torneio.create_games()
+        self.assertEqual(Jogo.objects.filter(torneio=self.torneio).count(), 0)
+
+    def test_game_status_concluido(self):
+        self.torneio.jogadores.add(self.player1, self.player2, self.player3, self.player4)
+        self.torneio.create_games()
+        jogo = Jogo.objects.filter(torneio=self.torneio).first()
+        jogo.placar_dupla1 = 6
+        jogo.placar_dupla2 = 7
+        jogo.save()
+        self.assertTrue(jogo.concluido == 'C')
