ea4c2ab11d6da41d93edad7b16b4cce12c8d3b48
andre23arruda <andre23arruda@gmail.com>
2026-02-12 10:04:55 -0300

    feat: Update api urls login

--- setup/urls.py
+++ setup/urls.py
@@ -10,13 +10,15 @@ from .views import (
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    # auth
+    path('api/check-auth', check_auth, name='check_auth'),
+    path('api/check-otp', check_otp, name='check_otp'),
+    path('api/csrf', get_csrf_token, name='get_csrf_token'),
+    path('api/staff-login', staff_login, name='staff_login'),
+    # apps
     path('api/torneio-v1/', include('bt_cup.urls')),
     path('api/torneio/', include('cup.urls')),
     path('api/rei-rainha/', include('bt_league.urls')),
-    path('check-auth', check_auth, name='check_auth'),
-    path('check-otp', check_otp, name='check_otp'),
-    path('csrf', get_csrf_token, name='get_csrf_token'),
-    path('staff-login', staff_login, name='staff_login'),
     path('quadras/', include('aluguel_quadra.urls')),
     path('escala-plantao/', include('escala_de_plantao.urls')),
 ]
